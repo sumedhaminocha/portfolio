6a1f4f1415a83fe12aed7cc2c102af8283d170b2
varunborar <varunborar@gmail.com>
2025-11-04 19:09:32 +0530

    update base path

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,6 @@ const isGithubActions = !!process.env.GITHUB_ACTIONS;
 
 let basePath = undefined;
 let assetPrefix = undefined;
-if (isGithubActions) {
-  const repo = (process.env.GITHUB_REPOSITORY || "").split("/")[1] || "";
-  if (repo) {
-    basePath = `/${repo}`;
-    assetPrefix = `/${repo}/`;
-  }
-}
 
 const nextConfig = {
   output: "export",
